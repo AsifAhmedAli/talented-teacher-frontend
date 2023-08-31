32c70b886a3049dab92d62c5c58cdfb9b5be07ef
AsifAhmedAli <asif@mexil.it>
2023-09-01 00:56:45 +0500

    fb pixels, active campaign tracking, update timer

--- assets/js/profile_third_party.js
+++ assets/js/profile_third_party.js
@@ -257,7 +257,7 @@ $.ajax({
 //   });
 // });
 
-var countDownDate = new Date("Sep 3, 2023 00:00:00").getTime();
+var countDownDate = new Date("Sep 17, 2023 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
